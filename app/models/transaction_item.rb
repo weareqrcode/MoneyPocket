890eef318888364dfb65731eb4591e3b3a0e226f
Book Chang <book770208@gmail.com>
2019-12-19 18:34:12 +0800

    fixed nested form

--- app/models/transaction_item.rb
+++ app/models/transaction_item.rb
@@ -4,6 +4,6 @@ class TransactionItem < ApplicationRecord
   validates :quantity, :price, :total, numericality: { greater_than: 0 }
   
   # Relationships
-  belongs_to :transaction_record, class_name: "Transaction", foreign_key: 'transaction_id', inverse_of: :transaction_items
+  belongs_to :transaction_record, class_name: "Transaction", foreign_key: 'transaction_id'
   # 讓transaction_record 指定找Model "Transaction" (因預設會找TransactionRecord)，並指定外鍵為"transaction_id"
 end 
