a00ac53692786cdbfca6478d91dd3d4ddc87bbf0
Book Chang <book770208@gmail.com>
2019-12-28 18:33:43 +0800

    remove jQuery 1.7.4 and fixed datepicker

--- app/frontend/packs/transactions.js
+++ app/frontend/packs/transactions.js
@@ -3,11 +3,11 @@ import $ from "jQuery";
 // import '../sweetalert.js';
 
 $(document).on('turbolinks:load', function() {
-  $('#start_date').off()
+  $('#start_date').off('click')
                   .on('click', function() {
                     flatpickr('#start_date', {})
                   });
-  $('#end_date').off()
+  $('#end_date').off('click')
                 .on('click', function() {
                   flatpickr('#end_date', {})
                 });
