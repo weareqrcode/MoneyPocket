947cdc5ccc91a9fadff1f1d990fb4072a3c47743
Book Chang <book770208@gmail.com>
2020-01-03 17:57:53 +0800

    fixed navbar button and login button

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "79a9455b.ngrok.io"
 end
