aa111ce23075f3a922f97edbea44c73ad5e96d7e
Eden Yang <dexuan90213@gmail.com>
2020-01-04 01:51:36 +0800

    set locale to zh_tw

--- app/frontend/packs/transactions.js
+++ app/frontend/packs/transactions.js
@@ -1,14 +1,15 @@
-import flatpickr from 'flatpickr'; // 別人的函式庫先上面，在放自己的js檔
-import $ from "jquery";
-// import '../sweetalert.js';
+import flatpickr from 'flatpickr'
+import { MandarinTraditional } from "flatpickr/dist/l10n/zh-tw"
 
-$(document).on('turbolinks:load', function() {
-  $('#start_date').off('click')
-                  .on('click', function() {
-                    flatpickr('#start_date', {})
-                  });
-  $('#end_date').off('click')
-                .on('click', function() {
-                  flatpickr('#end_date', {})
-                });
+console.log(MandarinTraditional)
+
+document.addEventListener('turbolinks:load', () => {
+  flatpickr.localize(MandarinTraditional)
+
+  flatpickr('#start_date', {})
+
+  flatpickr('#end_date', {
+    defaultDate: "today",
+    maxDate: "today",
+  })
 })
