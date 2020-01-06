4308ba2d67e7cfbb998621f8bde25595e8a34179
蕭凱文 <zozo2936@gmail.com>
2020-01-06 16:47:51 +0800

    input ThreeCodes OK

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -3,5 +3,11 @@ class ChecksController < ApplicationController
 
   def index
     @checks = Check.first.jsonb
+    respond_to do |check|
+      check.html { render "index" }
+      prize = @checks.select { |a, b| a =~ /No/ && b != ""}
+      prize_map = prize.map { |k, y| {:ID => k , :inter => y}}
+      check.json { render json: prize_map.to_json }
+    end
   end
 end
--- app/frontend/transactions/check.js
+++ app/frontend/transactions/check.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
-  $("#invot").on("chick", function(){
-    (".messege").removeClass("")
-  })
-  
-  
-  $("#submit").on("chick", function() {
-    let inv = $("#invot").val();
-    if (/d{3}$/.exec(inv)) {
-      (".messege").append(`<p>${inv}未中獎</p>`);
-    } else {
+  $("#submit").on("click", function() {
 
-    }
+    $.getJSON("/checks").then(date => {
+      let inv = $("#input").val();
+      $(".prize").remove("");
+      for (i = 0; i < 7; i++) {
+        $(".prize").remove("");
+        let string = date[i].inter.substr(-3);
+        if (inv == string) {
+          $(".messege").append(`<div class="prize">中獎 ${inv}</div>`)
+          $(".messege").append(`<div class="prize">繼續對對看  ${date[i].inter}</div>`);
+          {break;}
+        } else {
+          $(".messege").append(`<div class="prize">未中獎 ${inv}</div>`);
+        }
+      }
+      $("#input").val("");
+    });
   });
 });
