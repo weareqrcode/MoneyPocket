d2ac6e1ba543ef4d4b24bb525b9a0ee40e2c1074
蕭凱文 <zozo2936@gmail.com>
2020-01-06 16:50:28 +0800

    variable revise

--- app/frontend/transactions/check.js
+++ app/frontend/transactions/check.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $("#submit").on("click", function() {
 
-    $.getJSON("/checks").then(date => {
-      let inv = $("#input").val();
+    $.getJSON("/checks").then(data => {
+      let inv = $("#inv_input").val();
       $(".prize").remove("");
       for (i = 0; i < 7; i++) {
         $(".prize").remove("");
-        let string = date[i].inter.substr(-3);
+        let string = data[i].inter.substr(-3);
         if (inv == string) {
           $(".messege").append(`<div class="prize">中獎 ${inv}</div>`)
-          $(".messege").append(`<div class="prize">繼續對對看  ${date[i].inter}</div>`);
+          $(".messege").append(`<div class="prize">繼續對對看  ${data[i].inter}</div>`);
           {break;}
         } else {
           $(".messege").append(`<div class="prize">未中獎 ${inv}</div>`);
         }
       }
-      $("#input").val("");
+      $("#inv_input").val("");
     });
   });
 });
