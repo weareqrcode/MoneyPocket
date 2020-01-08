185853ec381f20d482bc6b8c282082dd28fb7ae0
cindyT <cindy0474@gmail.com>
2020-01-08 14:18:14 +0800

    registrations.js changed

--- app/frontend/packs/registrations.js
+++ app/frontend/packs/registrations.js
@@ -1,7 +1,6 @@
 document.addEventListener("turbolinks:load", () => {
   let url_string = window.location.href
   let url = new URL(url_string);
-  let c = url.searchParams.get('email');
-  console.log(c);
-  document.getElementById('inputEmail').setAttribute('value',c) ;
+  let mail = url.searchParams.get('email');
+  document.getElementById('inputEmail').setAttribute('value',mail) ;
 })
