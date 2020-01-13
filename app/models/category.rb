8cfd5fcb2d47e96b16603ce907ab9b09533b174f
BOOKCHANG <book770208@gmail.com>
2020-01-13 14:46:57 +0800

    add category function

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  # relationship
+  has_many :relations
+  has_many :transaction_items, through: 'relations'
 end
--- app/models/transaction_item.rb
+++ app/models/transaction_item.rb
@@ -5,4 +5,24 @@ class TransactionItem < ApplicationRecord
   
   # Relationships
   belongs_to :transaction_record, class_name: "Transaction", foreign_key: 'transaction_id'
+  has_many :relations
+  has_many :categories, through: 'relations'
+  
+  def category_list
+    categories.map(&:tag_name).join(', ')
+  end
+
+  def category_list=(tag_names)
+    self.categories = tag_names.split(',').map do |item|
+      Category.where(tag_name: item.strip).first_or_create!
+    end
+  end
+
+  def category_items
+    categories.map(&:tag_name)
+  end
+
+  def category_items=(tag_names)
+    self.categories = tag_names.map{|item| Category.where(tag_name: item.strip).first_or_create! unless item.blank?}.compact!
+  end
 end 
