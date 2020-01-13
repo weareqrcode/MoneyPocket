c7ce19d9b991eb99155392e8bf157e26ff17bff7
Eden Yang <dexuan90213@gmail.com>
2020-01-13 14:41:16 +0800

    remove development.rb ngrok hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "84e70856.ngrok.io"
 end
