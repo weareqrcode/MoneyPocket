af7a1220442bc4cb4249faa944f17b6febc2017c
BOOKCHANG <book770208@gmail.com>
2020-01-15 11:52:21 +0800

    add more btn background (WIP)

--- app/frontend/vue/more_btn_main/index.js
+++ app/frontend/vue/more_btn_main/index.js
@@ -2,7 +2,11 @@ const main = {
   data: {
     itemShow: false,
     itemId: '',
-    closeBtn: true
+    modalFade: 'modal fade',
+    modalBack: 'modal-backdrop fade',
+    show: 'show',
+    modalShow: false,
+    closeBtn: true,
   },
   methods: {
     expand(id) {
@@ -10,10 +14,12 @@ const main = {
       if (this.itemId === id) {
         this.itemId = ''
         this.itemShow = false
+        this.modalShow = false
       // click to open more
       } else {
         this.itemId = id
         this.itemShow = true
+        this.modalShow = true
       }
     },
     close() {
